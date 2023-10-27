71cbd95f7db8004f0d403c7e0aa3f80f6ca219e1
wasimalbokae2000 <wasimalbokae2000@gmail.com>
2023-10-27 07:11:13 +0300

    The first using by javascript (fourth task)

--- js/index.js
+++ js/index.js
@@ -9,3 +9,38 @@ function view_nav()
     x.className = "topnav";
   }
 }
+
+function submit_form(event) 
+{
+  event.preventDefault();
+  var from=document.querySelector("#first_select").options[first_select.selectedIndex].innerHTML;
+  var to=document.querySelector("#second_select").options[second_select.selectedIndex].innerHTML;
+  var date=document.querySelector("#date").value;
+if(from=="" || to==""||date=="")
+  alert("Check your data");
+else
+alert("Details your trip \n"+"From: "+from+"\nTo: "+to+"\nOn: "+date);
+}
+function change_selecetor(event)
+{
+  var second_select = document.querySelector('#second_select');
+  second_select.options[0].selected=true;
+  var first_index=event.target.selectedIndex;
+  for (let i = 1; i <second_select.options.length; i++) 
+  second_select.options[i].disabled=false;
+  second_select.options[first_index].disabled=true;
+}
+function change_bg(bg,event) 
+{
+ event.target.style.background = bg;
+}
+function email(event)
+{
+  var text=event.target.value;
+  const emailreg = /^([a-z0-9_\-\.]+)@([a-z0-9_\-\.]+)\.([a-z]{1,6})$/;
+  if (!emailreg.test(text))
+  alert("Your email format is incorrect.\nIt should be like (---@---.---) ");
+
+
+
+}
